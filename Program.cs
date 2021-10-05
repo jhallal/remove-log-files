6a77b46f0091b2619d76de1d1a48675aa20c0a6a
Jamil Hallal <jamilhallal@hotmail.com>
2021-10-05 12:22:01 +0300

    Initial creation

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveLogFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var files = new DirectoryInfo(ConfigurationManager.AppSettings["WebLogs"].ToString()).GetFiles("*.log");
+                foreach (var file in files)
+                {
+                    if (file.CreationTimeUtc < DateTime.Now.AddMonths(-6))
+                    {
+                        File.Delete(file.FullName);
+                    }
+                }
+            }
+            catch(Exception ex)
+            { 
+                //Log the exception
+            }
+        }
+    }
+}
